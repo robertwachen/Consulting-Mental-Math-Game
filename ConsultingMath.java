3b4d1dfbe146d2e936dcbea482a4bf528b475975
noschiff <noah@wow.com>
2020-07-26 01:02:38 -0400

    restructured everything

--- ConsultingMath.java
+++ ConsultingMath.java
@@ -1,11 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.text.DecimalFormat;
 
-public class ConsultingMath {
+public class ConsultingMath extends JFrame {
+    private JMenuBar mb_menu;
+    private JMenu m_file, m_help;
+    private JMenuItem mi_file_exit, mi_return_title;
+    private JMenuItem mi_help_about;
+
+    private JPanel currentPanel;
+    private JPanel titlePanel;
+
+    private final String ABOUT = "hello!!!";
+
+    private State state;
+
     private static JFrame mathFrame = new JFrame();
     private static JPanel mathPanel = new JPanel();
     private static JLabel problem = new JLabel();
@@ -13,259 +21,128 @@ public class ConsultingMath {
     private static JLabel answer = new JLabel("");
     private static String[] units = {"K", "M", "B", "T"};
     private static JButton nextQuestion = new JButton("Next Question");
-    private static String correctAnswer = "";
-    private static String question = "";
-    private static String game = "";
 
 
     public static void main(String[] args) {
-        createTitleFrame();
+        new ConsultingMath();
     }
 
-    public static void createTitleFrame() {
-        //defining elements
-        JFrame titleFrame = new JFrame("Consulting Math Tool");
-        JPanel mainPanel = new JPanel();
-        JPanel titlePanel = new JPanel();
-        JPanel subtitlePanel = new JPanel();
-        JPanel operationPanel = new JPanel();
-        JPanel operationButtons = new JPanel();
-        JLabel titleText = new JLabel("Consulting Mental Math Tool", SwingConstants.CENTER);
+    public ConsultingMath() {
+        //JFrame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Consulting Math Tool");
+        setSize(750, 750);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        //Menu
+        mb_menu = new JMenuBar();
+        setJMenuBar(mb_menu);
+        m_file = new JMenu("File");
+        mb_menu.add(m_file);
+        m_help = new JMenu("Help");
+        mb_menu.add(m_help);
+        mi_return_title = new JMenuItem("New Game");
+        mi_return_title.addActionListener(e -> setContentPane(titlePanel));
+        mi_file_exit = new JMenuItem("Exit");
+        mi_file_exit.addActionListener(e -> System.exit(0));
+        m_file.add(mi_return_title);
+        m_file.add(new JSeparator());
+        m_file.add(mi_file_exit);
+        mi_help_about = new JMenuItem("About");
+        mi_help_about.addActionListener(e -> JOptionPane.showMessageDialog(null, ABOUT, "About", JOptionPane.PLAIN_MESSAGE));
+        m_help.add(mi_help_about);
+
+        titlePanel = createTitlePanel();
+        setContentPane(titlePanel);
+        revalidate();
+        setVisible(true);
+        System.out.println("all good");
+    }
+
+    public JPanel createTitlePanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+
+        JLabel titleText = new JLabel("Consulting Mental Math Tool", JLabel.CENTER);
         JLabel subtitleText = new JLabel("By: Robert Wachen", SwingConstants.CENTER);
         JLabel operations = new JLabel("Pick which operation you'd like to practice:", SwingConstants.CENTER);
-        JButton multiply = new JButton();
-        JButton divide = new JButton();
-        JButton percent = new JButton();
 
-        //adding action listeners
-        multiply.addActionListener(e -> {
-            game = "multiply";
-            createMathFrame();
-            multiply();
-        });
-        divide.addActionListener(e -> {
-            game = "divide";
-            createMathFrame();
-            divide();
-        });
-        percent.addActionListener(e -> {
-            game = "percent";
-            createMathFrame();
-            percent();
-        });
+        panel.add(titleText);
+        panel.add(subtitleText);
+        panel.add(operations);
+        panel.add(buttonPanel);
 
-        //adding to layout
-        titleFrame.setSize(750, 750);
-        mainPanel.setLayout(new FlowLayout()); //change eventually
-        titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
-        subtitlePanel.setLayout(new BoxLayout(subtitlePanel, BoxLayout.Y_AXIS));
-        operationPanel.setLayout(new BoxLayout(operationPanel, BoxLayout.Y_AXIS));
-        operationButtons.setLayout(new FlowLayout());
+        //panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        Font buttonFont = new Font("SansSerif", Font.BOLD, 14);
+        State[] states = State.values();
+        JButton[] options = new JButton[states.length];
+        for (int i = 0; i < states.length; i++) {
+            State state = states[i];
+            options[i] = new JButton(state.toString());
+            options[i].setFont(buttonFont);
+            options[i].setVerticalAlignment(SwingConstants.CENTER);
+            options[i].addActionListener(e -> newMath(state));
+            buttonPanel.add(options[i]);
+        }
 
         //setting design
-        titleFrame.setBackground(Color.darkGray);
+        panel.setBackground(Color.green);
+        buttonPanel.setBackground(panel.getBackground());
 
         Font font = new Font("SansSerif", Font.BOLD, 30);
         titleText.setFont(font);
         titleText.setForeground(Color.blue);
-        titleText.setVerticalAlignment(SwingConstants.CENTER);
+        titleText.setAlignmentX(Container.CENTER_ALIGNMENT);
+        //titleText.setHorizontalAlignment(SwingConstants.CENTER);
 
         Font subtitleFont = new Font("SansSerif", Font.BOLD, 20);
         subtitleText.setFont(subtitleFont);
         subtitleText.setForeground(Color.blue);
-        subtitleText.setVerticalAlignment(SwingConstants.CENTER);
+       // subtitleText.setHorizontalAlignment(SwingConstants.CENTER);
 
         operations.setFont(new Font("SansSerif", Font.BOLD, 20));
         operations.setForeground(Color.blue);
-        operations.setVerticalAlignment(SwingConstants.CENTER);
-        operations.setAlignmentY(SwingConstants.CENTER);
+        //operations.setHorizontalAlignment(SwingConstants.CENTER);
+        //operations.setAlignmentY(SwingConstants.CENTER);
 
-        Font buttonFont = new Font("SansSerif", Font.BOLD, 14);
-        multiply.setText("Multiplication");
-        multiply.setFont(buttonFont);
-        multiply.setVerticalAlignment(SwingConstants.CENTER);
-        divide.setText("Division");
-        divide.setFont(buttonFont);
-        divide.setVerticalAlignment(SwingConstants.CENTER);
-        percent.setText("Percents");
-        percent.setFont(buttonFont);
-        percent.setVerticalAlignment(SwingConstants.CENTER);
-
-        titleFrame.addWindowListener(new WindowListener() {
-            @Override
-            public void windowOpened(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowClosing(WindowEvent e) {
-                int a = JOptionPane.showConfirmDialog(titleFrame, "Are you sure you want to leave?");
-                if (a == JOptionPane.YES_OPTION) {
-                    titleFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    System.exit(69);
-                }
-            }
-
-            @Override
-            public void windowClosed(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowIconified(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowDeiconified(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowActivated(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowDeactivated(WindowEvent e) {
-
-            }
-        });
-
-        //add to layout
-        titlePanel.add(titleText);
-        subtitlePanel.add(subtitleText, SwingConstants.CENTER);
-        operationPanel.add(operations, SwingConstants.CENTER);
-        operationButtons.add(multiply);
-        operationButtons.add(divide);
-        operationButtons.add(percent);
-
-        //add to main panel
-        mainPanel.add(titlePanel);
-        mainPanel.add(subtitlePanel);
-        mainPanel.add(operationPanel);
-        mainPanel.add(operationButtons);
-
-        //add to title frame and finish
-        titleFrame.add(mainPanel);
-        titleFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        titleFrame.setVisible(true);
+        return panel;
     }
 
-    public static void createMathFrame() {
-        Font font = new Font("SansSerif", Font.BOLD, 50);
-        problem.setFont(font);
-        problem.setForeground(Color.blue);
-        problem.setVerticalAlignment(SwingConstants.CENTER);
-
-        textField.setFont(font);
-        textField.setForeground(Color.blue);
-
-        answer.setFont(font);
-        answer.setForeground(Color.blue);
-        answer.setVerticalAlignment(SwingConstants.CENTER);
-
-        nextQuestion.setFont(font);
-        nextQuestion.setForeground(Color.blue);
-
-        problem.setPreferredSize(new Dimension(750, 150));
-        textField.setPreferredSize(new Dimension(750, 150));
-        answer.setPreferredSize(new Dimension(750, 150));
-        nextQuestion.setPreferredSize(new Dimension(750, 150));
-
-        mathPanel.add(problem);
-        mathPanel.add(textField);
-        mathPanel.add(answer);
-        mathPanel.add(nextQuestion);
-
-        mathPanel.setLayout(new BoxLayout(mathPanel, BoxLayout.Y_AXIS));
-        mathFrame.setLayout(new BoxLayout(mathFrame, BoxLayout.Y_AXIS));
-        mathFrame.setBackground(Color.darkGray);
-        mathFrame.setContentPane(mathPanel);
-        mathFrame.setSize(2500, 750);
-        mathFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        textField.addActionListener(e -> checkAnswer(e));
-        nextQuestion.addActionListener(e -> updateMathFrame(true));
-
-        mathFrame.setVisible(true);
+    private void newMath(State state) {
+        setContentPane(new MathPanel(state));
+        revalidate();
     }
 
-    public static void updateMathFrame(boolean nextProblem) {
-        if (nextProblem) {
-            if (game.equals("multiply")) {
-                multiply();
-            } else if (game.equals("divide")) {
-                divide();
-            } else if (game.equals("percent")) {
-                percent();
-            }
-        }
-        problem.setText(question);
-        textField.setText("");
-        answer.setText("");
+
+    public State getStateOfThis() {
+        return state;
     }
 
-    public static void checkAnswer(ActionEvent e) {
-        String theirAnswer = e.getActionCommand();
-        if (isNumeric(theirAnswer.substring(theirAnswer.length() - 1))
-                || !isNumeric(theirAnswer.substring(0, theirAnswer.length() - 1))) {
-            answer.setText("Invalid format. Please try again. Make sure to use units.");
-        } else {
-            if (theirAnswer.equals(correctAnswer)) {
-                answer.setText("Correct!");
-            } else {
-                answer.setText("Incorrect.\n Problem: " + question +
-                        "\n Your Answer: " + theirAnswer +
-                        "\n Correct Answer: " + correctAnswer);
-            }
-        }
+    public void setState(State state) {
+        this.state = state;
     }
 
-    public static void multiply() {
-        int number1 = (int) (((int) (Math.random() * 9.4 + 1) * 5) * (Math.pow(10, (int) (Math.random() * 5) + 1)));
-        int number2 = (int) (((int) (Math.random() * 9.4 + 1) * 5) * (Math.pow(10, (int) (Math.random() * 7) + 1)));
-        correctAnswer = formatNumber((double) number1 * number2);
-        question = "What is " + formatNumber(number1) + " times " + formatNumber(number2) + "?";
-        updateMathFrame(false);
-    }
+    public static void fitLabel(JLabel label) {
+        Font labelFont = label.getFont();
+        String labelText = label.getText();
 
-    public static void divide() {
-        long bigNumber = (long) (((int) (Math.random() * 98) + 1) * (Math.pow(10, (int) (Math.random() * 4) + 5)));
-        int littleNumber = (int) (((int) (Math.random() * 9.4 + 1) * 5) * (Math.pow(10, (int) (Math.random() * 5) + 1)));
-        correctAnswer = formatNumber((double) bigNumber / littleNumber);
-        question = "What is " + formatNumber(bigNumber) + " divided by " + formatNumber(littleNumber) + "?";
-        updateMathFrame(false);
-    }
+        int stringWidth = label.getFontMetrics(labelFont).stringWidth(labelText);
+        int componentWidth = label.getWidth();
 
-    public static void percent() {
-        int percent = (int) (Math.random() * 10) * 5;
-        long bigNumber = (long) (((int) (Math.random() * 98) + 1) * (Math.pow(10, (int) (Math.random() * 4) + 5)));
-        correctAnswer = formatNumber((double) Math.round((bigNumber * ((double) percent / 100)) * 100) / 100);
-        question = "What is " + percent + " percent of " + formatNumber(bigNumber) + "?";
-        updateMathFrame(false);
-    }
+// Find out how much the font can grow in width.
+        double widthRatio = (double)componentWidth / (double)stringWidth;
 
-    public static String formatNumber(double count) {
-        if (count < 1000) return "" + count;
-        long num = (long) count;
-        int exp = (int) (Math.log(num) / Math.log(1000));
-        DecimalFormat format = new DecimalFormat("0.##");
-        String value = format.format(num / Math.pow(1000, exp));
-        String number = String.format("%s%c", value, "KMBTPE".charAt(exp - 1));
-        return number;
-    }
+        int newFontSize = (int)(labelFont.getSize() * widthRatio);
+        int componentHeight = label.getHeight();
 
-    public static boolean isNumeric(String strNum) {
-        if (strNum == null) {
-            return false;
-        }
-        try {
-            double d = Double.parseDouble(strNum);
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-        return true;
-    }
+// Pick a new font size so it will not be larger than the height of label.
+        float fontSizeToUse = Math.min(newFontSize, componentHeight);
 
+// Set the label's font size to the newly determined size.
+        label.setFont(labelFont.deriveFont(fontSizeToUse));
+    }
 }
---
+++ MathPanel.java
@@ -0,0 +1,137 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.DecimalFormat;
+
+public class MathPanel extends JPanel {
+
+    //fonts
+    private static final Font font = new Font("SansSerif", Font.BOLD, 50);
+
+    private State state;
+    private final String stateInQuestion;
+
+    private String currentQuestion;
+    private String currentAnswer;
+
+    private JLabel problem, answer;
+    private JTextField textField;
+    private JButton next;
+    private JButton check;
+
+    public MathPanel(State state) {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.state = state;
+        stateInQuestion = state.getInQuestion();
+
+        problem = new JLabel("",JLabel.CENTER);
+        problem.setFont(font);
+        problem.setForeground(Color.blue);
+
+        textField = new JTextField();
+        textField.setFont(font);
+        textField.setForeground(Color.blue);
+
+        answer = new JLabel("dgf ",JLabel.CENTER);
+        answer.setForeground(Color.blue);
+        answer.setFont(font);
+        answer.setVerticalAlignment(SwingConstants.CENTER);
+
+        next = new JButton("Next Question");
+        next.setFont(font);
+        next.setForeground(Color.blue);
+        check = new JButton("Check Answer");
+        check.setFont(font);
+        check.setForeground(Color.blue);
+
+        problem.setSize(new Dimension(750, 150));
+        textField.setPreferredSize(new Dimension(750, 150));
+        answer.setPreferredSize(new Dimension(750, 150));
+        next.setPreferredSize(new Dimension(750, 150));
+
+        add(problem);
+        add(textField);
+        add(answer);
+        add(next);
+
+        textField.addActionListener(e -> checkAnswer(e.getActionCommand()));
+        check.addActionListener(e -> checkAnswer(e.getActionCommand()));
+        next.addActionListener(e -> nextQuestion());
+
+        nextQuestion();
+    }
+
+    private void nextQuestion() {
+        long num1, num2;
+        switch (state) {
+            case MULTIPLY:
+                num1 = (int) (((int) (Math.random() * 9.4 + 1) * 5) * (Math.pow(10, (int) (Math.random() * 5) + 1)));
+                num2 = (int) (((int) (Math.random() * 9.4 + 1) * 5) * (Math.pow(10, (int) (Math.random() * 7) + 1)));
+                currentAnswer = formatNumber((double) num1 * num2);
+                break;
+            case DIV:
+                num1 = (long) (((int) (Math.random() * 98) + 1) * (Math.pow(10, (int) (Math.random() * 4) + 5)));
+                num2 = (int) (((int) (Math.random() * 9.4 + 1) * 5) * (Math.pow(10, (int) (Math.random() * 5) + 1)));
+                currentAnswer = formatNumber((double) num1 / num2);
+                break;
+            case PERCENT:
+                num1 = (int) (Math.random() * 10) * 5;
+                num2 = (long) (((int) (Math.random() * 98) + 1) * (Math.pow(10, (int) (Math.random() * 4) + 5)));
+                currentAnswer = formatNumber((double) Math.round((num2 * ((double) num1 / 100)) * 100) / 100);
+                break;
+            default:
+                num1 = 0;
+                num2 = 0;
+        }
+        currentQuestion = "What is " + formatNumber(num1) + " " + stateInQuestion + " " + formatNumber(num2) + "?";
+        problem.setText(currentQuestion);
+        textField.setText("");
+        answer.setText("");
+        setBackground(Color.lightGray);
+        ConsultingMath.fitLabel(problem);
+    }
+
+    private static String formatNumber(double count) {
+        if (count < 1000) return String.valueOf(count);
+        long num = (long) count;
+        int exp = (int) (Math.log(num) / Math.log(1000));
+        DecimalFormat format = new DecimalFormat("0.##");
+        String value = format.format(num / Math.pow(1000, exp));
+        String number = String.format("%s%c", value, "KMBTPE".charAt(exp - 1));
+        return number;
+    }
+
+    public void checkAnswer(String input) {
+        if (isNumeric(input.substring(input.length() - 1))
+                || !isNumeric(input.substring(0, input.length() - 1))) {
+            answer.setText("Invalid format. Please try again. Make sure to use units.");
+            setBackground(Color.yellow);
+        } else {
+            if (input.equals(currentAnswer)) {
+                answer.setText("Correct!");
+                setBackground(Color.green);
+            } else {
+                answer.setText("Incorrect.\n Problem: " + currentQuestion +
+                        "\n Your Answer: " + input +
+                        "\n Correct Answer: " + currentAnswer);
+                setBackground(Color.red);
+            }
+        }
+        //ConsultingMath.fitLabel(answer);
+        System.out.println(answer.getSize());
+        System.out.println(answer.getFont());
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            Double.parseDouble(strNum);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+}
